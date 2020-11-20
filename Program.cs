33cafe1b34b76e4ef90754140b23d204fd33798b
monika-szymanska-macik <74740675+monika-szymanska-macik@users.noreply.github.com>
2020-11-20 05:49:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Scott's Grade Book");
+            while(true)
+            {
+                Console.WriteLine("Enter the grade. If you want to leave it, click on q");
+                var input = Console.ReadLine();
+                 if(input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+
+                    book.AddGrade(grade);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+
+            var stats = book.GetStatistics();
+
+            book.Name = "";
+
+            Console.WriteLine($"For thee book named {book.Name}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The average of grades is {stats.Average}");
+            Console.WriteLine($"The letter is {stats.Letter}");
+        }
+
+    }
+}
